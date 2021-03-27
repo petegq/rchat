a430abc0bef46bf76f16ac0616a710253a2dfe34
Peter Quin <peter@node.yoga>
2021-03-27 21:26:31 +0000

    Redirect to App on login

--- src/index.js
+++ src/index.js
@@ -4,20 +4,30 @@ import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from './firebase';
 
 import 'semantic-ui-css/semantic.min.css';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 
-const Root = () => (
-	<Router>
+const Root = (props) => {
+	firebase.auth().onAuthStateChanged(user => {
+		if (user) props.history.push('/')
+	});
+
+	return (
 		<Switch>
 			<Route exact path='/' component={App} />
 			<Route path='/login' component={Login} />
 			<Route path='/register' component={Register} />
 		</Switch>
-	</Router>
-);
+	)
+};
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
+	<Router>
+		<RootWithAuth />
+	</Router>, document.getElementById('root'));
 registerServiceWorker();
