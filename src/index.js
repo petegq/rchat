7a705d88c735938a559caa8618d3a0d7ff36aeb7
Peter Quin <peter@node.yoga>
2021-03-28 12:43:36 +0100

    Redirect to App on login fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
-import firebase from './firebase';
+import {Auth} from './firebase';
 
 import 'semantic-ui-css/semantic.min.css';
 
@@ -15,18 +15,20 @@ import {
 	withRouter,
 } from 'react-router-dom';
 
-const Root = props => {
-	firebase.auth().onAuthStateChanged(user => {
-		if (user) props.history.push('/');
+const Root = ({history}) => {
+	useEffect(() => {
+		Auth.onAuthStateChanged(user => {
+			if (user) history.push('/');
+		});
 	});
 
 	return (
 		<Switch>
-			<Route exact path='/' component={App} />
-			<Route path='/login' component={Login} />
+			<Route exact path='/' component={App}/>
+			<Route path='/login' component={Login}/>
 			<Route path='/register' component={Register} />
 		</Switch>
-	);
+	)
 };
 
 const RootWithAuth = withRouter(Root);
