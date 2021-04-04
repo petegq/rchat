8a73fd538e20af52c26c809b40a9cf13d7b63d80
Peter Quin <petegq@gmail.com>
2021-04-04 21:55:19 +0100

    Add signout button

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const App = () => {
-	return <div>App</div>;
+import { Auth } from '../firebase';
+
+const App = ({history}) => {
+	const signOut = () => {
+		return Auth.signOut()
+			.then(() => history.push('/login'));
+	}
+	return (
+		<div>
+			<button onClick={() => signOut()}>Sign Out</button>
+		</div>
+	);
 };
 
 export default App;
