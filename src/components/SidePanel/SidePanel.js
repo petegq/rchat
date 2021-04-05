80d8cda859a94da35fce41b26aab9e2200af0336
Peter Quin <petegq@gmail.com>
2021-04-05 23:18:51 +0100

    Add UserPanel

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Menu } from 'semantic-ui-react';
+import UserPanel from './UserPanel';
 
-const SidePanel = () => <div>SidePanel</div>;
+const SidePanel = () => (
+	<Menu
+		size={'large'}
+		inverted
+		fixed={'left'}
+		vertical
+		style={{ background: '#449', fontSize: '1.2rem' }}>
+		<UserPanel />
+	</Menu>
+);
 
 export default SidePanel;
---
+++ src/components/SidePanel/UserPanel.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Grid, Header } from 'semantic-ui-react';
+
+const UserPanel = () => (
+	<Grid style={{ background: '#499' }}>
+		<Grid.Column>
+			<Grid.Row>
+				<Header></Header>
+			</Grid.Row>
+		</Grid.Column>
+	</Grid>
+);
+
+export default UserPanel;
