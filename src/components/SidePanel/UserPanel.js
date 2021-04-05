7fa6ee8700a7771d887a9a72014eb5a973d12ab9
Peter Quin <petegq@gmail.com>
2021-04-05 23:25:58 +0100

    Icon and invert color

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Grid, Header } from 'semantic-ui-react';
+import { Grid, Header, Icon } from 'semantic-ui-react';
 
 const UserPanel = () => (
 	<Grid style={{ background: '#449' }}>
 		<Grid.Column>
 			<Grid.Row style={{ margin: 0, padding: '1.2em' }}>
-				<Header floated={'left'} as={'h2'}>
+				<Header inverted floated={'left'} as={'h2'}>
+					<Icon name={'wechat'} />
 					<Header.Content>rChat</Header.Content>
 				</Header>
 			</Grid.Row>
