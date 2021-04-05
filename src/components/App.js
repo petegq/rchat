3d7b6bca1c6e9b324f2419463c0c01988c98e655
Peter Quin <petegq@gmail.com>
2021-04-05 22:44:31 +0100

    Add first chat panel

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Grid } from 'semantic-ui-react';
+import SidePanel from './SidePanel/SidePanel';
 
 import { Auth } from '../firebase';
 
-const App = ({history}) => {
+const App = ({ history }) => {
 	const signOut = () => {
-		return Auth.signOut()
-			.then(() => history.push('/login'));
-	}
+		return Auth.signOut().then(() => history.push('/login'));
+	};
 	return (
-		<div>
-			<button onClick={() => signOut()}>Sign Out</button>
-		</div>
+		// <div>
+		// 	<button onClick={() => signOut()}>Sign Out</button>
+		// </div>
+		<Grid
+			columns={'equal'}
+			className={'app'}
+			style={{ background: '#999' }}>
+			<SidePanel />
+		</Grid>
 	);
 };
 
