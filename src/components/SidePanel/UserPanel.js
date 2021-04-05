7a98aad5ce8c152df68168181886fea1c916ea05
Peter Quin <petegq@gmail.com>
2021-04-05 23:23:22 +0100

    Header and title

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -4,8 +4,10 @@ import { Grid, Header } from 'semantic-ui-react';
 const UserPanel = () => (
 	<Grid style={{ background: '#449' }}>
 		<Grid.Column>
-			<Grid.Row>
-				<Header></Header>
+			<Grid.Row style={{ margin: 0, padding: '1.2em' }}>
+				<Header floated={'left'} as={'h2'}>
+					<Header.Content>rChat</Header.Content>
+				</Header>
 			</Grid.Row>
 		</Grid.Column>
 	</Grid>
