6199c93e4ee0469e9939740d09b15b6a6c496660
Peter Quin <petegq@gmail.com>
2021-04-05 21:18:56 +0100

    Add rootReducer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
-import {Auth} from './firebase';
+import { Auth } from './firebase';
 
 import 'semantic-ui-css/semantic.min.css';
 
@@ -18,10 +18,11 @@ import {
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
 
-const store = createStore(() => {}, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools());
 
-const Root = ({history}) => {
+const Root = ({ history }) => {
 	useEffect(() => {
 		Auth.onAuthStateChanged(user => {
 			if (user) history.push('/');
@@ -30,11 +31,11 @@ const Root = ({history}) => {
 
 	return (
 		<Switch>
-			<Route exact path='/' component={App}/>
-			<Route path='/login' component={Login}/>
+			<Route exact path='/' component={App} />
+			<Route path='/login' component={Login} />
 			<Route path='/register' component={Register} />
 		</Switch>
-	)
+	);
 };
 
 const RootWithAuth = withRouter(Root);
